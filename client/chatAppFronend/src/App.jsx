00e02b826281830213936f62dfb93efc700bb270
Aman Dwivedi <amandwivedi262@gmail.com>
2026-05-14 02:35:22 +0530

    Fix case-sensitive imports

--- client/chatAppFronend/src/App.jsx
+++ client/chatAppFronend/src/App.jsx
@@ -1,27 +1,27 @@
-import { useState } from 'react'
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Login from "./pages/Login.jsx"
-import Register from "./pages/Register.jsx"
-import './App.css'
-import ProtectedRoute from './components/ProtectedRoute.jsx'
-import Chat from './pages/chat.jsx'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import Chat from "./pages/Chat.jsx";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Login/>} />
-      <Route path='/register' element={<Register/>} />
-      <Route path='/chat' element={
-        <ProtectedRoute>
-        <Chat/>
-        </ProtectedRoute>
-        } />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/chat"
+          element={
+            <ProtectedRoute>
+              <Chat />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
